f0d731e59bd3bb0417c1646f85a2c1b3cc6c73ba
milenakosh <147971990+milenakosh@users.noreply.github.com>
2024-09-28 14:44:00 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let n = 17;
+let d = 19;
+let s = 25;
+n *= 2.54;
+d *= 2.54;
+s *= 2.54;
+console.log(n);
+console.log(d);
+console.log(s);
+
+let f = 123456789;
+f %= 3;
+console.log(f);
+
+let sum2 = 500 + 600 + 700;
+console.log(sum2);
+
+
+
+
+
+
+
+let count = 3;
+++count;
+console.log(count);
+
+
+
